af151b13b0f1f173f8f2ca496638c9a3e72a1d38
sutti <suttipong.ta@rmuti.ac.th>
2024-01-07 21:27:13 +0700

    add btn go to repo

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,14 @@ function App() {
   return (
     <div className="app flex justify-center items-center ">
       <TodoApp />
+      <div className="flex justify-end items-end absolute right-4 bottom-4">
+        <a
+          href="https://github.com/64332110128-7/Homework14.git"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded inline-block"
+        >
+          Go to my Repo
+        </a>
+      </div>
     </div>
   )
 }
